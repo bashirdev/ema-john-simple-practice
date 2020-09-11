d244001c4f9eb9c1092a545ceef8e6ff500864e8
bashir ahammed <bahammed16@gmail.com>
2020-09-11 14:40:27 +0400

    shipment form added

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,10 +1,27 @@
 import React from 'react';
-
+import { useForm } from "react-hook-form";
+import './Shipment.css';
 const Shipment = () => {
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => console.log(data);
+  
+    console.log(watch("example")); // watch input value by passing the name of it
+  
     return (
-        <div>
-            <h1>This is shipment</h1>
-        </div>
+
+      <form className="ship-form" onSubmit={handleSubmit(onSubmit)}>
+              <input name="name" ref={register({ required: true })} placeholder='Name'/>
+              {errors.name && <span className="error">Name field is required</span>}
+              <input name="email" ref={register({ required: true })} placeholder='Email'/>
+              {errors.email && <span className="error">Email field is required</span>}
+              <input name="address" ref={register({ required: true })} placeholder='Address'/>
+              {errors.address && <span className="error">Address field is required</span>}
+              <input name="phone" ref={register({ required: true })} placeholder='Phone'/>
+              {errors.phone && <span className="error">Phone field is required</span>}
+     
+        
+        <input className='input'  type="submit" />
+      </form>
     );
 };
 
